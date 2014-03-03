3afb3f8e7b829143bf403c6ab97123157a11139d
Keith Busch <keith.busch@intel.com>
2014-03-03 11:09:47 -0700

    NVMe: Initialize device reference count earlier
    If an NVMe device becomes ready but fails to create IO queues, the driver
    creates a character device handle so the device can be managed. The
    device reference count needs to be initialized before creating the
    character device.
    
    Signed-off-by: Keith Busch <keith.busch@intel.com>
    Signed-off-by: Matthew Wilcox <matthew.r.wilcox@intel.com>

--- drivers/block/nvme-core.c
+++ drivers/block/nvme-core.c
@@ -2455,6 +2455,7 @@ static int nvme_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 	if (result)
 		goto release;
 
+	kref_init(&dev->kref);
 	result = nvme_dev_start(dev);
 	if (result) {
 		if (result == -EBUSY)
@@ -2462,7 +2463,6 @@ static int nvme_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 		goto release_pools;
 	}
 
-	kref_init(&dev->kref);
 	result = nvme_dev_add(dev);
 	if (result)
 		goto shutdown;
