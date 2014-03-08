2e5ccab9dd05f029137271d006d6222ba0bbdb44
Valentina Manea <valentina.manea.m@gmail.com>
2014-03-08 14:53:34 +0200

    staging: usbip: claim ports used by shared devices
    A device should not be able to be used concurrently both by
    the server and the client. Claiming the port used by the
    shared device ensures no interface drivers bind to it and
    that it is not usable from the server.
    
    Signed-off-by: Valentina Manea <valentina.manea.m@gmail.com>
    Acked-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/usb.h
+++ include/linux/usb.h
@@ -370,6 +370,8 @@ struct usb_bus {
 #endif
 };
 
+struct dev_state;
+
 /* ----------------------------------------------------------------------- */
 
 struct usb_tt;
@@ -753,6 +755,11 @@ extern struct usb_host_interface *usb_find_alt_setting(
 		unsigned int iface_num,
 		unsigned int alt_num);
 
+/* port claiming functions */
+int usb_hub_claim_port(struct usb_device *hdev, unsigned port1,
+		struct dev_state *owner);
+int usb_hub_release_port(struct usb_device *hdev, unsigned port1,
+		struct dev_state *owner);
 
 /**
  * usb_make_path - returns stable device path in the usb tree
