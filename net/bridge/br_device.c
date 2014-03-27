10a03757fc67b98fcfaa1d12079900dca1ab65ca
Toshiaki Makita <makita.toshiaki@lab.ntt.co.jp>
2014-03-27 21:46:55 +0900

    bridge: Fix inabillity to retrieve vlan tags when tx offload is disabled
    Bridge vlan code (br_vlan_get_tag()) assumes that all frames have vlan_tci
    if they are tagged, but if vlan tx offload is manually disabled on bridge
    device and frames are sent from vlan device on the bridge device, the tags
    are embedded in skb->data and they break this assumption.
    Extract embedded vlan tags and move them to vlan_tci at ingress.
    
    Signed-off-by: Toshiaki Makita <makita.toshiaki@lab.ntt.co.jp>
    Acked-by: Vlad Yasevich <vyasevic@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/bridge/br_device.c
+++ net/bridge/br_device.c
@@ -44,19 +44,20 @@ netdev_tx_t br_dev_xmit(struct sk_buff *skb, struct net_device *dev)
 	}
 #endif
 
-	if (!br_allowed_ingress(br, br_get_vlan_info(br), skb, &vid))
-		goto out;
-
 	BR_INPUT_SKB_CB(skb)->brdev = dev;
 
 	skb_reset_mac_header(skb);
 	skb_pull(skb, ETH_HLEN);
+
+	if (!br_allowed_ingress(br, br_get_vlan_info(br), skb, &vid))
+		goto out;
 
 	u64_stats_update_begin(&brstats->syncp);
 	brstats->tx_packets++;
 	/* Exclude ETH_HLEN from byte stats for consistency with Rx chain */
 	brstats->tx_bytes += skb->len;
 	u64_stats_update_end(&brstats->syncp);
+
 
 	if (is_broadcast_ether_addr(dest))
 		br_flood_deliver(br, skb, false);
