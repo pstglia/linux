c69c755a3b6b9c13eade28331a33788b3a1f7763
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2014-05-26 13:40:47 +0200

    PM / sleep: Add state field to pm_states[] entries
    commit 27ddcc6596e50cb8f03d2e83248897667811d8f6 upstream.
    
    To allow sleep states corresponding to the "mem", "standby" and
    "freeze" lables to be different from the pm_states[] indexes of
    those strings, introduce struct pm_sleep_state, consisting of
    a string label and a state number, and turn pm_states[] into an
    array of objects of that type.
    
    This modification should not lead to any functional changes.
    
    Change-Id: I457bc0fbce141434aa0bc9e598b309e674548471
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Cc: Brian Norris <computersforpeace@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/kernel/power/main.c
+++ kernel/kernel/power/main.c
@@ -298,7 +298,7 @@ static ssize_t state_show(struct kobject *kobj, struct kobj_attribute *attr,
 	suspend_state_t i;
 
 	for (i = PM_SUSPEND_MIN; i < PM_SUSPEND_MAX; i++)
-		if (pm_states[i].state)
+		if (valid_state(i))
 			s += sprintf(s,"%s ", pm_states[i].label);
 
 #endif
@@ -334,9 +334,8 @@ static suspend_state_t decode_state(const char *buf, size_t n)
 
 #ifdef CONFIG_SUSPEND
 	for (s = &pm_states[state]; state < PM_SUSPEND_MAX; s++, state++)
-		if (s->state && len == strlen(s->label)
-		    && !strncmp(buf, s->label, len))
-			return s->state;
+		if (len == strlen(s->label) && !strncmp(buf, s->label, len))
+			return state;
 #endif
 
 	return PM_SUSPEND_ON;
@@ -461,7 +460,7 @@ static ssize_t autosleep_show(struct kobject *kobj,
 
 #ifdef CONFIG_SUSPEND
 	if (state < PM_SUSPEND_MAX)
-		return sprintf(buf, "%s\n", pm_states[state].state ?
+		return sprintf(buf, "%s\n", valid_state(state) ?
 					pm_states[state].label : "error");
 #endif
 #ifdef CONFIG_HIBERNATION
