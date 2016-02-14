183dee26c32b6277a434cd2a36be67de61e9eec3
Paulo Sergio Travaglia <pstglia@gmail.com>
2016-02-14 09:54:59 -0200

    Adds AMCR0F28 as a valid machine driver
    Includes AMCR0F28 as a valid machine driver
    Also adds WM510205 as acpi dev id

--- sound/soc/intel/sst-acpi.c
+++ sound/soc/intel/sst-acpi.c
@@ -249,6 +249,7 @@ static struct sst_acpi_mach baytrail_machines[] = {
 	{ "10EC5640", "byt-rt5640", "intel/fw_sst_0f28.bin-48kHz_i2s_master" },
 	{ "193C9890", "byt-max98090", "intel/fw_sst_0f28.bin-48kHz_i2s_master" },
 	{ "WM510204", "byt_wm5102", "intel/fw_sst_0f28.bin-48kHz_i2s_master" },
+	{ "WM510205", "byt_wm5102", "intel/fw_sst_0f28.bin-48kHz_i2s_master" },
 	{}
 };
 
@@ -267,7 +268,7 @@ static struct acpi_device_id sst_acpi_match[] = {
 	{ "INT33C8", (unsigned long)&sst_acpi_haswell_desc },
 	{ "INT3438", (unsigned long)&sst_acpi_broadwell_desc },
 	{ "80860F28", (unsigned long)&sst_acpi_baytrail_desc },
-	{ }
+	{ "AMCR0F28", (unsigned long)&sst_acpi_baytrail_desc }
 };
 MODULE_DEVICE_TABLE(acpi, sst_acpi_match);
 
