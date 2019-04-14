2464763cbdf6d236447c95b86a717f9767fb50a7
Paulo Sergio Travaglia <pstglia@gmail.com>
2019-04-14 09:32:57 -0300

    Initial commit (kernel T113NU)
    Downloaded from http://opensource.samsung.com [1]
    
    [1] - http://opensource.samsung.com/reception/receptionSub.do?method=sub&sub=F&searchValue=t113
    
    Change-Id: I934ce68d53e46d90200f7410591b171adc0ce779

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 5 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#5 SMP PREEMPT Tue Dec 23 21:05:38 KST 2014"
+#define LINUX_COMPILE_BY "jinwook81.song"
+#define LINUX_COMPILE_HOST "wookuntu"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
